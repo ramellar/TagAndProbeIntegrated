d8e776bdc79c045da95cd1edc00fcbbc0faafca9
mchiusi <marco.chiusi@cern.ch>
2025-03-05 16:48:24 +0100

    updating resubmitting bash

--- TagAndProbe/test/resubmitFileOpenError.sh
+++ TagAndProbe/test/resubmitFileOpenError.sh
@@ -1,12 +1,36 @@
-user=$( pwd | cut -d"/" -f4)
+#!/bin/bash
 
-for logfile in log*.txt
+# Check if an argument is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <path_to_directory>"
+    exit 1
+fi
+
+target_dir=$1
+
+# Check if the directory exists
+if [ ! -d "$target_dir" ]; then
+    echo "Error: Directory $target_dir does not exist."
+    exit 1
+fi
+
+# Loop through subdirectories
+for dir in "$target_dir"
 do
-   tmp=${logfile#*_}
-   idx=${tmp%.*}
-   out=$( grep FileOpenError $logfile )
-   if [[ $out == "An exception of category 'FileOpenError' occurred while" ]]; then
-      echo "resubmitting job num $idx"
-      /home/llr/cms/$user/t3submit -short job_$idx.sh
-   fi
+    cd "$dir" || continue  # Avoid errors if cd fails
+    user=$(pwd | cut -d"/" -f4)
+
+    # Loop through log files
+    for logfile in log*.txt
+    do
+        tmp=${logfile#*_}
+        idx=${tmp%.*}
+        out=$(grep -r "FileOpenError" "$logfile")
+        
+        if [[ $out == "An exception of category 'FallbackFileOpenError' occurred while" ]]; then
+            echo "Resubmitting job num $idx"
+            /home/llr/cms/$user/t3submit -short "job_$idx.sh"
+        fi
+    done
+    cd - > /dev/null  # Return to the previous directory, suppress output
 done
