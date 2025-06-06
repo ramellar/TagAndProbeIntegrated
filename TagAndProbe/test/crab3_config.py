d5c65b87ac8fde06de9452d3d4ed5805b8113af6
Ruth Amella Ranz <ruthamella83@gmail.com>
2025-06-06 15:51:31 +0200

    crab submission for MC

--- TagAndProbe/test/crab3_config.py
+++ TagAndProbe/test/crab3_config.py
@@ -4,27 +4,27 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'TauTagAndProbe_2025C_mu0'
+config.General.requestName = 'MC2025_DY_CMSSW_15'
 config.General.workArea = 'Crab3WorkArea'
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'tau_tagAndProbeRun3.py'
+config.JobType.psetName = 'tau_noTagAndProbe.py'
 config.JobType.allowUndistributedCMSSW = True
 
 config.section_("Data")
-config.Data.inputDataset = '/Muon0/Run2025C-PromptReco-v1/MINIAOD'
-config.Data.secondaryInputDataset= '/Muon0/Run2025C-v1/RAW'
+config.Data.inputDataset = '/DYto2Tau-4Jets_Bin-MLL-50_Fil-MuTauh_TuneCP5_13p6TeV_madgraphMLM-pythia8/Run3Winter25MiniAOD-142X_mcRun3_2025_realistic_v7-v2/MINIAODSIM'
+config.Data.secondaryInputDataset= '/DYto2Tau-4Jets_Bin-MLL-50_Fil-MuTauh_TuneCP5_13p6TeV_madgraphMLM-pythia8/Run3Winter25Digi-142X_mcRun3_2025_realistic_v7-v2/GEN-SIM-RAW'
 
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'EventAwareLumiBased'
 config.Data.unitsPerJob = 25000 #number of events per jobs
 config.Data.totalUnits = -1 #number of event
-config.Data.outLFNDirBase = '/store/group/dpg_trigger/comm_trigger/L1Trigger/athachay/crabOutputStorage/2025/Data/'
-config.Data.runRange = '392672-392997'
-config.Data.lumiMask = 'Collisions25_13p6TeV_Latest.json'
+config.Data.outLFNDirBase = '/store/user/ramellar/tau_run3_2025'
+# config.Data.runRange = '392672-392997'
+# config.Data.lumiMask = '../DataCertificationJsons/Collisions25_13p6TeV_Latest.json'
 config.Data.publication = False
 config.Data.allowNonValidInputDataset = True
 #config.Data.outputDatasetTag = 'TagAndProbe_TagAndProbe_RelValZEE_13UP16_2016_LATEST'
 config.section_("Site")
-config.Site.storageSite = 'T2_CH_CERN'
+config.Site.storageSite = 'T2_FR_GRIF_LLR'
---
+++ TagAndProbe/test/local.py
@@ -0,0 +1,3 @@
+outLFNDirBase = '/store/user/ramellar/'
+storageSite = 'T2_FR_GRIF_LLR'
+prod_name = 'TauTagAndProbe_2025C'
